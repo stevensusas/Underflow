21c36b37eef8c2f0cc8bce2b92d6760ca85a97fc
Laurence Liang <laurence.w.liang@gmail.com>
2024-09-21 11:58:45 -0400

    WIP

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"data": "Stack Underflow"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, port=8000)
