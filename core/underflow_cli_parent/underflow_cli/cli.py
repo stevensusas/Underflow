aabdb8863a2a49081db77fd326911ced5cba072f
stevensusas <hanqisu@sas.upenn.edu>
2024-09-21 18:24:53 -0400

    cli

--- core/underflow_cli_parent/underflow_cli/cli.py
+++ core/underflow_cli_parent/underflow_cli/cli.py
@@ -1,11 +1,13 @@
 import click
 import requests
 from underflow_sdk.analyze_code import check_for_external_services
-
+import json
 
 @click.command
 @click.argument("uri")
-def cli(uri: str):
+@click.argument("traffic")
+def cli(uri: str, traffic: int):
     code_str = requests.get(uri).text
-    resp = check_for_external_services(code_str=code_str)
-    print(resp.choices[0].message.content)
+    resp = check_for_external_services(code_str=code_str, traffic=traffic)
+    print("Your Optimized Tech Stack:" + json.dumps(resp[1], indent=4))
+    print("Technical Report:" + json.dumps(resp[2], indent=4))
--- core/underflow_sdk_parent/underflow_sdk/analyze_code.py
+++ core/underflow_sdk_parent/underflow_sdk/analyze_code.py
@@ -3,11 +3,11 @@ import os
 from cerebras.cloud.sdk import Cerebras
 from dotenv import load_dotenv
 from pprint import pprint
+import requests
 
 load_dotenv()
 
 cerebras_client = Cerebras(api_key=os.environ.get("CEREBRAS_API_KEY"))
-
 
 def get_hello_world():
     resp = cerebras_client.chat.completions.create(
@@ -16,7 +16,7 @@ def get_hello_world():
     return resp
 
 
-def check_for_external_services(code_str: str):
+def check_for_external_services(code_str: str, traffic: int):
     resp = cerebras_client.chat.completions.create(
         messages=[
             {
@@ -28,8 +28,23 @@ def check_for_external_services(code_str: str):
         model="llama3.1-8b",
         temperature=0.0,
     )
-    return resp
 
+    url_original_service = "http://localhost:8000/original-services"
+    url_optimal_service = "http://localhost:8000/optimal-services"
+    url_tech_report = "http://localhost:8000/generate-report"
+
+    services = resp.choices[0].message.content.split(", ")
+
+    payload = {
+    "services": services,
+    "traffic": traffic
+}
+
+    original_service = requests.post(url_original_service, json = payload).json()
+    optimal_service = requests.post(url_optimal_service, json = payload).json()
+    tech_report = requests.post(url_tech_report, json = payload).json()
+
+    return (original_service, optimal_service, tech_report)
 
 if __name__ == "__main__":
     pprint(get_hello_world().choices[0].message.content)
